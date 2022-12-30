f3e41194e311551b2e633ec644683cc5ef326ce8
Jesse Krensel <jdkrensel@gmail.com>
2022-12-29 19:10:48 -0500

    update port for heroku deployment

--- app.js
+++ app.js
@@ -134,6 +134,6 @@ app.get("/:customListName", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000\n");
 });
